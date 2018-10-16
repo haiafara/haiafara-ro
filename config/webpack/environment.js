d056b9147b82bec8e5ae14978255a1baca70e010
Janos Rusiczki <janos.rusiczki@gmail.com>
2018-10-16 17:55:31 +0200

    Fix webpacker to work with Vuetify. * https://github.com/lukebyrne/rails-vuetify-test/commit/b6870ddf708b12a7e389faf9ba79d586e38ab3df * https://github.com/vuetifyjs/vuetify/issues/4068#issuecomment-393021003

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,10 @@
 const { environment } = require('@rails/webpacker')
+const customConfig = require('./custom')
+
 const vue =  require('./loaders/vue')
+
+// merge custom config
+environment.config.merge(customConfig)
 
 environment.loaders.append('vue', vue)
 module.exports = environment
