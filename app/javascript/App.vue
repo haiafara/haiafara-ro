2256035d1b5ae478fa8ab889e927cd7cbb7da138
Janos Rusiczki <janos.rusiczki@gmail.com>
2019-01-19 03:00:32 +0200

    Refactor JS application to use components. Map refreshes do not yet work. [skip ci]

---
+++ app/javascript/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" fixed right app>
+      <v-list dense>
+        <v-list-tile @click="focusTown">
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Tot orașul</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="focusChurch">
+          <v-list-tile-action>
+            <v-icon>contact_mail</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Biserica Romano-Catolică</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar color="light-blue" dense dark app>
+      <v-toolbar-title>Hartă Turistică Baia Sprie</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-side-icon @click.stop="toggleNavigationDrawer"></v-toolbar-side-icon>
+    </v-toolbar>
+    <v-content>
+      <haiafara-map></haiafara-map>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+  import Map from './Map.vue'
+
+  export default {
+    components: {
+      'haiafara-map': Map
+    },
+    data: function() {
+      return {
+        drawer: false
+      }
+    },
+    methods: {
+      toggleNavigationDrawer: function (event) {
+        this.drawer = !this.drawer;
+        setTimeout(function() {
+          // map.invalidateSize(true)
+        }, 200);
+      },
+      focusTown: function (event) {
+        // map.setView([47.6623, 23.6970], 15);
+      },
+      focusChurch: function (event) {
+        // map.setView([47.66283, 23.69984], 18);
+      }
+    },
+  }
+</script>
---
+++ app/javascript/Map.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="map"></div>
+</template>
+
+<script>
+  import L from 'leaflet'
+  import 'leaflet/dist/leaflet.css'
+
+  export default {
+    mounted: function() {
+      this.$nextTick(function () {
+        var map = L.map("map", { maxZoom: 20, trackResize: true }).setView([47.6623, 23.6970], 15);
+        var tileLayer = L.tileLayer("//tileserver.link7.ro/hot/{z}/{x}/{y}.png", {
+          attribution: '&copy; Contribuitori <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+          maxZoom: 20
+        });
+        tileLayer.addTo(map);
+      })
+    }
+  }
+</script>
+
+<style>
+  #map {
+    z-index: 0;
+    width: 100%;
+    height: 100%;
+  }
+</style>
