7277d5581ad8593fba539a9bf78c46c1c99d64c4
Janos Rusiczki <janos.rusiczki@gmail.com>
2019-01-21 02:23:38 +0200

    Seed database with first region & zone.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+region = Region.create(name: 'Maramures')
+zone = Zone.create(name: 'Baia Sprie', region: region, default: true)
