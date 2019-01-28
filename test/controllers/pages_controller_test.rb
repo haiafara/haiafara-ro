e784ce0b7cf6cbbb6186dc99a5d201ef1486f7a0
Janos Rusiczki <janos.rusiczki@gmail.com>
2019-01-29 00:39:39 +0200

    Remove test (MiniTest) directory

--- test/controllers/pages_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class PagesControllerTest < ActionDispatch::IntegrationTest
-  test "should redirect to default zone" do
-    get pages_home_url
-    assert_redirected_to zone_url(zones(:two).slug)
-  end
-end
--- test/models/region_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class RegionTest < ActiveSupport::TestCase
-  test 'valid region' do
-    region = Region.new
-    assert region.valid?
-  end
-end
