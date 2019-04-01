9470d6b238dc4c2611045a529e11e133ac32df65
Janos Rusiczki <janos.rusiczki@gmail.com>
2019-04-01 21:45:09 +0300

    Update POI’s shape field after save

--- app/models/poi.rb
+++ app/models/poi.rb
@@ -3,4 +3,12 @@ class POI < ApplicationRecord
   friendly_id :name, use: :slugged
 
   has_and_belongs_to_many :zones
+
+  after_save :update_shape
+
+  private
+
+  def update_shape
+    GetPOIShapeJob.perform_later(self.id)
+  end
 end
