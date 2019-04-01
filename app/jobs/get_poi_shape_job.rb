524e2f58a817aef20776149bdc2514602545bdca
Janos Rusiczki <janos.rusiczki@gmail.com>
2019-04-01 21:44:42 +0300

    No need to require underpass it’s autoloaded

--- app/jobs/get_poi_shape_job.rb
+++ app/jobs/get_poi_shape_job.rb
@@ -1,8 +1,6 @@
 class GetPOIShapeJob < ApplicationJob
   queue_as :default
 
-  require 'underpass'
-
   def perform(poi_id)
     poi = POI.find(poi_id)
     poi.zones.with_expanded_bounding_box.each do |z|
