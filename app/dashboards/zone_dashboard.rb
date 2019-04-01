2e4957cd0ce5ae4f9522c1b9b19f21e003b129ac
Janos Rusiczki <janos.rusiczki@gmail.com>
2019-04-01 21:43:16 +0300

    Display zone name on admin pages

--- app/dashboards/zone_dashboard.rb
+++ app/dashboards/zone_dashboard.rb
@@ -54,7 +54,7 @@ class ZoneDashboard < Administrate::BaseDashboard
   # Overwrite this method to customize how zones are displayed
   # across all pages of the admin dashboard.
   #
-  # def display_resource(zone)
-  #   "Zone ##{zone.id}"
-  # end
+  def display_resource(zone)
+    "#{zone.name}"
+  end
 end
