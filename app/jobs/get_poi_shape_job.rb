a451f8357c03408fa79937eba4a2cfedbcac9ed5
Janos Rusiczki <janos.rusiczki@gmail.com>
2019-04-02 20:32:29 +0200

    Refactor POI shape job and specs

--- app/jobs/get_poi_shape_job.rb
+++ app/jobs/get_poi_shape_job.rb
@@ -2,17 +2,6 @@ class GetPOIShapeJob < ApplicationJob
   queue_as :default
 
   def perform(poi_id)
-    poi = POI.find(poi_id)
-    poi.zones.with_expanded_bounding_box.each do |z|
-      matches = Underpass::QL::Query.perform(
-        z.expanded_bounding_box,
-        poi.overpass_query
-      )
-      if matches.size
-        poi.update_column(:shape, matches.first)
-        break
-      end
-    end
-    # if not found, send email
+    POIService.update_shape_from_overpass(poi_id)
   end
 end
--- spec/models/poi_spec.rb
+++ spec/models/poi_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe POI, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { described_class.new }
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'triggers update_shape on save' do
+    expect(subject).to receive(:enqueue_update_shape)
+    subject.save
+  end
+
+  describe '#enqueue_update_shape' do
+    it 'calls GetPOIShapeJob' do
+      expect(GetPOIShapeJob).to receive(:perform_later)
+      subject.send(:enqueue_update_shape)
+    end
+  end
 end
