081e5bc5ffd9ea4df463fe64d539adcfaeb24991
Janos Rusiczki <janos.rusiczki@gmail.com>
2019-04-02 22:06:13 +0300

    Fix failing spec

--- spec/models/poi_spec.rb
+++ spec/models/poi_spec.rb
@@ -8,14 +8,14 @@ RSpec.describe POI, type: :model do
   end
 
   it 'triggers update_shape on save' do
-    expect(subject).to receive(:enqueue_update_shape)
+    expect(subject).to receive(:enqueue_update_shape_job)
     subject.save
   end
 
-  describe '#enqueue_update_shape' do
+  describe '#enqueue_update_shape_job' do
     it 'calls POIUpdateShapeJob' do
       expect(POIUpdateShapeJob).to receive(:perform_later)
-      subject.send(:enqueue_update_shape)
+      subject.send(:enqueue_update_shape_job)
     end
   end
 end
