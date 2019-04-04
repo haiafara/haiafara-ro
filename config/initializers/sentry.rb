a985af98926dbee0fc71f8457207f4d17a5251a3
Janos Rusiczki <janos.rusiczki@gmail.com>
2019-04-04 14:25:11 +0200

    The app will still bot when missing the sentry.io config file

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,4 +1,5 @@
 Raven.configure do |config|
-  config.dsn = YAML.load_file("#{Rails.root.to_s}/config/sentry.yml")['dsn']
+  config_file = "#{Rails.root.to_s}/config/sentry.yml"
+  config.dsn = File.exists?(config_file) ? YAML.load_file(config_file)['dsn'] : nil
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
 end
