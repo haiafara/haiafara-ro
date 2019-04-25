9ede3d1efa3f0d0cee25b7a8a71fc79d19c35e7a
Janos Rusiczki <janos.rusiczki@gmail.com>
2019-04-25 11:14:52 +0200

    Fix broken migrations

--- db/migrate/20190130005848_add_overpass_query_to_pois.rb
+++ db/migrate/20190130005848_add_overpass_query_to_pois.rb
@@ -1,4 +1,4 @@
-class AddOverpassQueryToPois < ActiveRecord::Migration[5.2]
+class AddOverpassQueryToPOIs < ActiveRecord::Migration[5.2]
   def change
     add_column :pois, :overpass_query, :text
   end
