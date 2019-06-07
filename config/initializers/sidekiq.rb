761d3749efa326d02d19397161911aa8c9283b8a
Janos Rusiczki <janos.rusiczki@gmail.com>
2019-06-07 15:46:43 +0200

    Have sidekiq use a specific database when defined in ENV

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,9 @@
+redis_database = ENV['REDIS_DATABASE'].present? ? ENV['REDIS_DATABASE'].to_s : '2'
+
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://127.0.0.1:6379/2' }
+  config.redis = { url: "redis://127.0.0.1:6379/#{redis_database}" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://127.0.0.1:6379/2' }
+  config.redis = { url: "redis://127.0.0.1:6379/#{redis_database}" }
 end
