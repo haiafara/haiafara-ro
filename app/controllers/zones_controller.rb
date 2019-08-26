f3cb5e813f27a18983ce9fbdbc3c73079f7129c5
Janos Rusiczki <janos.rusiczki@gmail.com>
2019-08-26 15:02:58 +0200

    Move zone loading into a concern

--- app/controllers/zones_controller.rb
+++ app/controllers/zones_controller.rb
@@ -1,10 +1,6 @@
 class ZonesController < ApplicationController
+  include ZoneLoader
   def show
-    @zone = Zone.includes(:pois).friendly.find(params[:id])
     @page_title = 'Hartă Turistică ' + @zone.name
-    @resource_json = ZoneSerializer.new(
-      @zone,
-      include: [:pois]
-    ).serialized_json
   end
 end
