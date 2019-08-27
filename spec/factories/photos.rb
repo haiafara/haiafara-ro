d2761b1657929897ea328c2a33770448d6780f79
Janos Rusiczki <janos.rusiczki@gmail.com>
2019-08-27 16:40:36 +0200

    PhotoSerializer spec

--- spec/factories/photos.rb
+++ spec/factories/photos.rb
@@ -1,7 +1,11 @@
 FactoryBot.define do
   factory :photo do
-    name { "MyString" }
-    description { "MyText" }
+    sequence :name do |n|
+      "Test Photo #{n}"
+    end
+    sequence :description do |n|
+      "Test Description #{n}"
+    end
     image { File.open('support/haiafara-screenshot.png') }
     photoable { nil }
   end
