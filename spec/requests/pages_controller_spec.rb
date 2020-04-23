cdab9cf4bf88c881073a3e23648f0e2e6f7bdb1e
Janos Rusiczki <janos.rusiczki@gmail.com>
2020-04-23 11:25:10 +0000

    Make Rubocop / Codacy happy

--- spec/requests/pages_controller_spec.rb
+++ spec/requests/pages_controller_spec.rb
@@ -1,39 +1,39 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe PagesController, type: :request do
   describe 'GET /' do
-    subject { get root_path }
+    subject(:root_path_request) { get root_path }
 
     context 'when a default zone exists' do
       let!(:zone) { create(:zone, default: true) }
 
       it 'redirects to the default zone' do
-        expect(subject).to redirect_to(zone_path(zone))
+        expect(root_path_request).to redirect_to(zone_path(zone))
       end
     end
 
     context 'when a zone exists but it is not default' do
-      let!(:zone) { create(:zone) }
+      before { create(:zone) }
 
       it 'redirects to the no zone page' do
-        expect(subject).to redirect_to(pages_no_zone_path)
+        expect(root_path_request).to redirect_to(pages_no_zone_path)
       end
     end
 
     context 'when no zones were created' do
-      let!(:zone) { create(:zone) }
-
       it 'redirects to the no zone page' do
-        expect(subject).to redirect_to(pages_no_zone_path)
+        expect(root_path_request).to redirect_to(pages_no_zone_path)
       end
     end
   end
 
   describe 'GET /pages/no_zone' do
-    before(:each) { get pages_no_zone_path }
+    before { get pages_no_zone_path }
 
     it 'responds with 200' do
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(:ok)
     end
 
     it 'responds with the correct content type' do
