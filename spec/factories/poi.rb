e94eb2d6f6ed4e07662b7d977c82c9be1ec831c3
Janos Rusiczki <janos.rusiczki@gmail.com>
2020-09-29 14:04:56 +0000

    Make rubocop ignore factory length

--- spec/factories/poi.rb
+++ spec/factories/poi.rb
@@ -5,22 +5,27 @@ FactoryBot.define do
     sequence :name do |n|
       "Test POI #{n}"
     end
+
     sequence :description do |n|
       "Test POI Description #{n}"
     end
+
     trait :with_zones do
       transient do
         zone_count { 1 }
       end
+
       zones do
         Array.new(zone_count, build(:zone))
       end
     end
+
     trait :point do
       shape do
         '0020000001000010e64037b33d07c84b5e4047d5ef73c0c1fd'
       end
     end
+
     trait :with_description_html do
       sequence :description_html do |n|
         "Test POI Description HTML #{n}"
